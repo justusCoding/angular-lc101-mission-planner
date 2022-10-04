0b358246bad2a668fb5f6c2ebb3af07f719475f8
justus solomon <arjack2o0@gmail.com>
2022-10-04 15:13:39 -0500

    angular studio 2

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -6,16 +6,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./crew.component.css']
 })
 export class CrewComponent implements OnInit {
-
+  memberBeingEdited: object = null;
   crew: object[] = [
     {name: "Eileen Collins", firstMission: false},
     {name: "Mae Jemison", firstMission: false},
-    {name: "Ellen Ochoa", firstMission: true}
+    {name: "Ellen Ochoa", firstMission: true},
+    {name: "bob marlley", firstMission: false},
+    {name: "deez nuts", firstMission: true}
+
   ];
 
   constructor() { }
 
   ngOnInit() {
   }
+
+
+  add(memberName: string, isFirst: boolean) {
+    this.crew.push({name: memberName, firstMission: isFirst});
+  }
+
+  remove(member: object) {
+    let index = this.crew.indexOf(member);
+    this.crew.splice(index, 1);
+  }
+  
+
+  edit(member: object) {
+    this.memberBeingEdited = member;
+ }
+
+ save(name: string, member: object) {
+  member['name'] = name;
+  this.memberBeingEdited = null;
+  }
+
 
 }
